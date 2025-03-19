3b24db907680418af2b1b6245f23a4751f4e40b2
Victor Castaneda <vidanilo10@gmail.com>
2025-03-18 19:06:05 -0500

    Remove local storage

--- app/src/main/java/com/abc/enrut/enrut/data/local/entity/TokenEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.abc.enrut.enrut.data.local.entity
-
-import androidx.room.Entity
-import androidx.room.ColumnInfo
-
-@Entity(tableName = "token")
-data class TokenEntity(
-    @ColumnInfo(name="value") var value: String? = ""
-)
--- app/src/main/java/com/abc/enrut/enrut/presentation/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/abc/enrut/enrut/presentation/viewmodel/UserViewModel.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.abc.enrut.enrut.data.repository.UserRepositoryImp
-import com.abc.enrut.enrut.domain.model.TokenModel
 import com.abc.enrut.enrut.domain.model.UserAuth
 import kotlinx.coroutines.launch
 
@@ -26,11 +25,7 @@ class UserViewModel: ViewModel() {
                 val response = UserRepositoryImp.api.login(auth=userAuthInst)
                 println(response)
 
-                val tokenModel = TokenModel(
-                    value = response.idToken
-                )
 
-                println(tokenModel)
             } catch (e: Exception) {
                 throw e
             }
