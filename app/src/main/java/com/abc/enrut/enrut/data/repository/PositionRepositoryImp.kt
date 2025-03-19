c9ac39a56210c4459f4623353c5e09b9d6c747af
Victor Castaneda <vidanilo10@gmail.com>
2025-03-18 19:42:30 -0500

    Clean position repository

--- app/src/main/java/com/abc/enrut/enrut/data/repository/PositionRepositoryImp.kt
+++ app/src/main/java/com/abc/enrut/enrut/data/repository/PositionRepositoryImp.kt
@@ -1,11 +1,13 @@
 package com.abc.enrut.enrut.data.repository
 import com.abc.enrut.enrut.domain.repository.PositionRepository
+import com.abc.enrut.enrut.core.Constants
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object PositionRepositoryImp {
-    private val BASE_URL = "https://skrfbs1bok.execute-api.us-east-2.amazonaws.com/dev"
+    private val BASE_URL = Constants.hostname
 
     val api: PositionRepository by lazy {
         Retrofit.Builder()
